2234aebb59c990695d3d16a665b18298e0e4dd2d
keyurs8589 <keyurs8589@gmail.com>
2020-06-24 16:57:53 +1000

    store issue

--- src/employeeRedux/store.js
+++ src/employeeRedux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
 import empReducer from './empReducer';
 import thunk from 'redux-thunk';
-const store = createStore(empReducer, undefined, applyMiddleware(thunk));
+const store = createStore(empReducer, applyMiddleware(thunk));
 export default store;
